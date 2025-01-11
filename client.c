44bebeed4fe66b85f943326d13de418d85ade6a3
Marjorie <marjorie_4.28@hotmail.com>
2025-01-11 15:15:26 +0100

    work of 11.01.25

--- client.c
+++ client.c
@@ -13,14 +13,14 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-void    send_signal(int pid, unsigned char c)
+void    send_signal(pid_t pid, unsigned char c)
 {
-    int i;
+    int bit;
 
-    i = 0;
-    while (i < 8)
+    bit = 0;
+    while (bit < 8)
     {
-        if ((c >> i) & 1) 
+        if ((c >> bit) & 1) 
             kill(pid, SIGUSR2);
         else
             kill(pid, SIGUSR1);
@@ -29,10 +29,15 @@ void    send_signal(int pid, unsigned char c)
     }
 }
 
-int main(int argv, char **argv)
+int main(int argc, char **argv)
 {
+    int i;
+
+    i = 0;
     if (argc != 3)
-        return (1);
+        retrun (1);
+    serverPID = ft_atoi(argv[1][i]);
+    message = argv[2][i];
     return (0);
 }
 
@@ -42,6 +47,8 @@ careful.
 ** 1 char is equal to 1 octet and it'e equal to 8 bites : the char 'a' is 97 
 in 10 base or 01100001 in binary. Every char is equal to 8 bites.
 ** '>>' est un decalage binaire. Shift right every bites c to i positions.
-** '&' is a binary comparator, read every bit and check if it a 1 or 0
+** '&' is a binary comparator, read every bit and check if it a 1 or 0.
+** '|' is a binary comparator, it compare if two bites and check if it the
+same or different. return (1) if it's true or return (0)
 ** 3 argc : 1. programm's name, 2. PID, 3. the message. So we want exatly
 3 args */
--- server.c
+++ server.c
@@ -14,7 +14,21 @@
 #include "ft_printf.h"
  
  void   handle_signal(signal)
- {}
+ {
+   static char c;
+   static int  bit;
+
+   c = c | (signal == SIGUSR1);
+   bit++;
+   if (bit == 8)
+   {
+      bit = 0;
+      if ()
+         ft_printf ("\n");
+      else
+         ft_printf("%c", c);
+   }
+ }
 
  int    main (void)
  {
